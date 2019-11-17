c1c6d984725adc62dc3759118e29087a6f4fb1a7
labosis <onlyfowow@gmail.com>
2019-11-17 06:56:57 +0200

    events added

---
+++ FileTraker.ps1
@@ -0,0 +1,21 @@
+$downloads = "$home\Downloads"
+$filesToWatch = "**"
+
+$WatherProperties = @{
+  Path   = $downloads;
+  Filter = $filesToWatch;
+  NotifyFilter = [IO.NotifyFilters]"FileName, LastWrite"
+  IncludeSubdirectories = $false; 
+}
+
+$FileChangeHandler = {
+  $name = $Event.SourceEventArgs.Name 
+  $changeType = $Event.SourceEventArgs.ChangeType 
+  $timeStamp = $Event.TimeGenerated 
+  Write-Host "The file '$name' was $changeType at $timeStamp" 
+}
+
+$FileChangeNotifier = New-Object IO.FileSystemWatcher -Property $WatherProperties
+Register-ObjectEvent $FileChangeNotifier Created -SourceIdentifier FileCreated -Action $FileChangeHandler
+Register-ObjectEvent $FileChangeNotifier Deleted -SourceIdentifier FileDeleted -Action $FileChangeHandler
+Register-ObjectEvent $FileChangeNotifier Changed -SourceIdentifier FileChanged -Action $FileChangeHandler
